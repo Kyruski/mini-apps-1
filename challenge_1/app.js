b1494dd58b1a69444a910039ec37a88b20b0c614
kyruski <jamescboyett@gmail.com>
2019-01-28 13:34:54 -0600

    Adds checkForWin function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,15 @@
 const defaultSpace = '&#183;',
       spaces       = document.getElementsByClassName("space"),
       score        = {x: 0, o: 0},
-      blankSpace   = '&nbsp;';
+      blankSpace   = '&nbsp;',
+      winLogic     = [[0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]];
 let   turn         = true; // True is for X, False is for O
 
 function initialize () {
@@ -35,6 +43,24 @@ function resetBoard () {
   for (let item of spaces) {
     item.innerHTML = defaultSpace;
   }
+}
+
+function makeBoardArray () {
+  const boardArray = [];
+  for (let item of spaces) {
+    boardArray.push(item.innerHTML);
+  }
+  return boardArray;
+}
+
+function checkForWin () {
+  const boardArray = makeBoardArray();
+  for (let combo of winLogic) {
+    if (spaces[combo[0]].innerHTML === spaces[combo[1]].innerHTML && spaces[combo[1]].innerHTML === spaces[combo[2]].innerHTML) {
+      return true;
+    }
+  }
+  return false;
 }
 
 initialize();
