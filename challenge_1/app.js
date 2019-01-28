489774f425d4f97e6d57eeeb9ee6df70658dbc60
kyruski <jamescboyett@gmail.com>
2019-01-28 11:54:23 -0600

    Implements a placeInSpace and part of an onClick function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,13 @@
 
 
+
+
+function placeInSpace (location, player) {
+  location.value = player;
+}
+
+function onClick () {
+  if (this.value === 'X' || this.value === 'O') {
+    document.getElementsByID("messages").value = 'That is an invalid move. Please try another space.';
+  }
+}
