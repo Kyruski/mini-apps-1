c4de54de79ffcd7f4d05ce1743bc67e6ee72d650
kyruski <jamescboyett@gmail.com>
2019-01-28 12:22:13 -0600

    Fixes the way event listeners are added

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,12 @@
+const defaultSpace = '&#183;';
 
 
+const spaces = document.getElementsByClassName("space")
+for (let item of spaces) {
+  item.addEventListener("click", onClick);
+}
 
+document.getElementById("reset-board").addEventListener("click", resetBoard);
 
 function placeInSpace (location, player) {
   location.value = player;
@@ -9,5 +15,14 @@ function placeInSpace (location, player) {
 function onClick () {
   if (this.value === 'X' || this.value === 'O') {
     document.getElementsByID("messages").value = 'That is an invalid move. Please try another space.';
+  } else {
+    console.log('I got clicked');
+  }
+}
+
+function resetBoard () {
+  for (let item of spaces) {
+    item.value = 'defaultSpace';
+    console.log('set space: ', item, ' to ', defaultSpace);
   }
 }
