f6fa66cddf86d5256250285096c5ce07c7d76382
kyruski <jamescboyett@gmail.com>
2019-01-28 13:14:00 -0600

    Adds the ability to keep track of the turn and play in a space; Adds a default message to the message area in the HTML

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,28 +2,32 @@ const defaultSpace = '&#183;',
       spaces       = document.getElementsByClassName("space"),
       score        = {x: 0, o: 0},
       blankSpace   = '&nbsp;';
+let turn = true; // True is for X, False is for O
 
 function initialize () {
   for (let item of spaces) {
     item.addEventListener("click", onClick);
   }
-  
   document.getElementById("reset-board").addEventListener("click", resetBoard);
 }
 
-function placeInSpace (location, player) {
-  location.innerHTML = player;
+function placeInSpace (location) {
+  location.innerHTML = (turn) ? 'X' : 'O';
 }
 
 function onClick () {
-  if (this.value === 'X' || this.value === 'O') {
+  if (this.innerHTML === 'X' || this.innerHTML === 'O') {
     document.getElementById("messages").innerHTML = 'That is an invalid move. Please try another space.';
   } else {
-    document.getElementById("messages").innerHTML = 'I got clicked';
+    placeInSpace(this);
+    turn = !turn;
+    document.getElementById("messages").innerHTML = (turn) ? 'It is X\'s turn.' : 'It is O\'s turn.';
   }
 }
 
 function resetBoard () {
+  document.getElementById("messages").innerHTML = 'It is X\'s turn.';
+  turn = true;
   for (let item of spaces) {
     item.innerHTML = defaultSpace;
   }
