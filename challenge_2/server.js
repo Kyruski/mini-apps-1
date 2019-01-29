f827ee7400127fbf002765c2e36855110ee012f8
kyruski <jamescboyett@gmail.com>
2019-01-29 11:25:08 -0600

    Implements a function to collect all of the values of the obj into an array of arrays, with one array for each obj

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -10,3 +10,24 @@ function createKeyArray (obj) {
   return keyArray;
 }
 
+function createValueArrays (obj) {
+  const valuesArray = [];
+  const makeRowArray = (location) => { //this function makes an array of values for a row (obj) and pushes it into a return array
+    let rowArray = [];
+    for (let key of obj) { //iterates over the keys and adds values to the individual row array;
+      if(Array.isArray(obj[key])) { //if the value is an array, we're at the end of the object, push the item and then check if there are children to run the function on
+        valuesArray.push(rowArray);
+        rowArray = [];
+        if (obj[key].length) {
+          for (let item of obj[key]) {
+            makeRowArray(item);
+          }
+        }
+      } else {
+        rowArray.push(obj[key]);
+      }
+    }
+  }
+  makeRowArray(obj);
+  return valuesArray;
+}
