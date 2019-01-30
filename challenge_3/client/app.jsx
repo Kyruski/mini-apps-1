cf68d70efa415c9bfc38a61ce8e7b90607522bb3
kyruski <jamescboyett@gmail.com>
2019-01-30 12:25:47 -0600

    Adds the button and functionality on each component to move to the next form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,22 +1,57 @@
 
 const MakeOrder = (props) => {
-  return (<div>Make an Order</div>);
+  return (
+    <div>
+      Make and Order
+      <div>
+        <a href="#" onClick={props.changeForm()}>Go to the next form</a>
+      </div>
+    </div>
+    );
 };
 
 const Form1 = (props) => {
-  return (<div>Form 1</div>);
+  return (
+    <div>
+      Form 1
+      <div>
+        <a href="#" onClick={props.changeForm()}>Go to the next form</a>
+      </div>
+    </div>
+    );
 };
 
 const Form2 = (props) => {
-  return (<div>Form 2</div>);
+  return (
+    <div>
+      Form 2
+      <div>
+        <a href="#" onClick={props.changeForm()}>Go to the next form</a>
+      </div>
+    </div>
+    );
 };
 
 const Form3 = (props) => {
-  return (<div>Form 3</div>);
+  return (
+    <div>
+      Form 3
+      <div>
+        <a href="#" onClick={props.changeForm()}>Go to the next form</a>
+      </div>
+    </div>
+    );
 };
 
 const OrderComplete = (props) => {
-  return (<div>Your Order is complete!</div>);
+  return (
+    <div>
+      Your Order is Complete!
+      <div>
+        <a href="#" onClick={props.changeForm()}>Go to the next form</a>
+      </div>
+    </div>
+    );
 };
 
 class App extends React.Component {
@@ -37,22 +72,31 @@ class App extends React.Component {
     }
   }
 
+  changeForm() {
+    return () => {
+      let nextForm = (this.state.currentForm + 1) % 5;
+      this.setState({
+        currentForm: nextForm
+      })
+    };
+  }
+
   render() {
     let renderItem;
     switch (this.state.currentForm) {
       case 1:
-        renderItem = <Form1 />;
+        renderItem = <Form1 changeForm={this.changeForm.bind(this)} />;
         break;
       case 2:
-        renderItem = <Form2 />;
+        renderItem = <Form2 changeForm={this.changeForm.bind(this)} />;
         break;
       case 3:
-        renderItem = <Form3 />;
+        renderItem = <Form3 changeForm={this.changeForm.bind(this)} />;
         break;
       case 4:
-        renderItem = <OrderComplete />;
+        renderItem = <OrderComplete changeForm={this.changeForm.bind(this)} />;
         break;
-      default: renderItem = <MakeOrder />
+      default: renderItem = <MakeOrder changeForm={this.changeForm.bind(this)} />;
     }
     return <div>{renderItem}</div>;
   }
