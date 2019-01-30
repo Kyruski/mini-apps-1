9e89cbbe0661858e2de1efc048c086072b824902
kyruski <jamescboyett@gmail.com>
2019-01-30 12:07:28 -0600

    Adds the Dynamically changing React component for changing pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,30 @@
 
+const MakeOrder = (props) => {
+  return (<div>Make an Order</div>);
+};
+
+const Form1 = (props) => {
+  return (<div>Form 1</div>);
+};
+
+const Form2 = (props) => {
+  return (<div>Form 2</div>);
+};
+
+const Form3 = (props) => {
+  return (<div>Form 3</div>);
+};
+
+const OrderComplete = (props) => {
+  return (<div>Your Order is complete!</div>);
+};
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      currentForm: 0,
       form1vars: {
 
       },
@@ -17,20 +38,24 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>Hello</div>
+    let renderItem;
+    switch (this.state.currentForm) {
+      case 1:
+        renderItem = <Form1 />;
+        break;
+      case 2:
+        renderItem = <Form2 />;
+        break;
+      case 3:
+        renderItem = <Form3 />;
+        break;
+      case 4:
+        renderItem = <OrderComplete />;
+        break;
+      default: renderItem = <MakeOrder />
+    }
+    return <div>{renderItem}</div>;
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-const form1 = (props) => {
-
-};
-
-const form2 = (props) => {
-
-};
-
-const form3 = (props) => {
-
-};
