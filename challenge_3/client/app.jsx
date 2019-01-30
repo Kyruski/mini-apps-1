73d21bdaca6d8606cc7ee6621ac96ec8e9587513
kyruski <jamescboyett@gmail.com>
2019-01-30 11:09:10 -0600

    Adds the classes in the app.jsx file to fill out

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,33 @@
 
 
+class App extends React.Components {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      form1vars: {
+        
+      },
+      form2vars: {
+
+      },
+      form3vars: {
+
+      }
+    }
+  }
+}
+
+
+const form1 = (props) => {
+
+};
+
+const form2 = (props) => {
+
+};
+
+const form3 = (props) => {
+
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXX0=
