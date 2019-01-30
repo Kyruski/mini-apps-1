d9a10fc7acc3f6875a2f104f0d95c4a3439583e6
kyruski <jamescboyett@gmail.com>
2019-01-29 18:07:51 -0600

    Implements posting the csv to the html page

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,15 @@
 //takes in JSON
 //returns CSV
-const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
 
 function createKeyArray (obj) {
   const keyArray = [];
-  for (let key of obj) {
-    keyArray.push(key);
+  for (let key in obj) {
+    if (!Array.isArray(obj[key])) {
+      keyArray.push(key);
+    }
   }
   return keyArray;
 }
@@ -17,21 +18,22 @@ function createValueArrays (obj) {
   const valuesArray = [];
   const makeRowArray = (location) => { //this function makes an array of values for a row (obj) and pushes it into a return array
     let rowArray = [];
-    for (let key of obj) { //iterates over the keys and adds values to the individual row array;
-      if(Array.isArray(obj[key])) { //if the value is an array, we're at the end of the object, push the item and then check if there are children to run the function on
+    for (let key in location) { //iterates over the keys and adds values to the individual row array;\
+      if(Array.isArray(location[key])) { //if the value is an array, we're at the end of the object, push the item and then check if there are children to run the function on
         valuesArray.push(rowArray);
         rowArray = [];
-        if (obj[key].length) {
-          for (let item of obj[key]) {
+        if (location[key].length) {
+          for (let item of location[key]) {
             makeRowArray(item);
           }
         }
       } else {
-        rowArray.push(obj[key]);
+        rowArray.push(location[key]);
       }
     }
   }
   makeRowArray(obj);
+  console.log('valuearray', valuesArray);
   return valuesArray;
 }
 
@@ -41,26 +43,39 @@ function writeCSV (arrayToConvert, returnArray) {
   return returnArray;
 }
 
-function main (obj) {
-  fileCounter++;
+function createCSV (obj) {
+  console.log('createCSV obj1', obj);
   const keysArray = createKeyArray(obj);
   const valuesArray = createValueArrays(obj);
   let finalArray = writeCSV(keysArray, []);
   for (let item of valuesArray) {
     finalArray = (writeCSV(item, finalArray));
   }
-  //return file?
+  return finalArray;
 }
-
-app.use(express.static( __dirname + '/client'))
+app.engine('pug', require('pug').__express)
+app.set('views', __dirname);
+app.set('view engine', 'pug');
+app.use(express.static('client'));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extend: true}));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(pathName, 'client', 'index.html'));
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(pathName, 'client', 'index.html'));
+// })
 
 app.post('/', (req, res) => {
-  console.log(req.body.payload);
+  let jsonObject;
+  try {
+    jsonObject = JSON.parse(req.body.payload);
+    // res.json({data: csvArray});
+  } catch (e) {
+    console.log("not JSON");
+    res.end('Not valid JSON');
+  }
+  const csvArray = createCSV(jsonObject);
+  // res.append('Link', ['<http://localhost/>', '<http://localhost:3000/>']);
+  res.render('index', {csv: csvArray});
+  
 })
 app.listen(3000, () => {console.log('Listening to port 3000')});
