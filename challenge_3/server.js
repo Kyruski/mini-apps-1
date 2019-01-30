ab743a3cad25a6b1e14e7d17bb40273470afe4b0
kyruski <jamescboyett@gmail.com>
2019-01-30 09:48:34 -0600

    Sets up the server to listen and serve static elements

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const port = 3000;
 
+app.use(static('client'));
+app.use(bodyParser.json());
 
+app.listen(port);
